32a8444af36d07a07590d5cffe3517936cbf5efd
antonk-prog <kornilov.anthon@yandex.ru>
2024-11-11 12:03:05 +0300

    started

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
+define('APP_PATH', __DIR__);
+
+require_once APP_PATH . '/vendor/autoload.php';
+
+use App\App;
+
+$app = new App();
+
+$app->run();
+
+// dd(APP_PATH);
